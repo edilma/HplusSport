5a97d24932f633e1dc5a2da46f59325509a1d867
Edilma Riano <edilmaria@hotmail.com>
2021-05-03 17:14:22 -0400

    pagination, filtering and searching ready.  Working on sorting

---
+++ HplusSport.API/Classes/QueryParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HplusSport.API.Classes
+{
+    public class QueryParameters
+    {
+        // Maximum size is 100
+        const int _maxSize = 100;
+
+        // This is my default size
+        private int _size = 50;
+
+        public int Page { get; set; }
+        public int Size
+        {
+            get
+            {
+                return _size;
+
+            }
+
+            // This is for when the user provides a size.  We allow maximum 100
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+    }
+}
--- HplusSport.API/Controllers/ProductsController.cs
+++ HplusSport.API/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HplusSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+using HplusSport.API.Classes;
 
 namespace HplusSport.API.Controllers
 {
@@ -21,11 +23,78 @@ namespace HplusSport.API.Controllers
             _context.Database.EnsureCreated();
         }
 
+        // Method 1 -> get the products in a list
+        //[HttpGet]   
+        //public IEnumerable<Product>GetAllProducts() 
+        //{
+        //    return _context.Products.ToArray();
+        //}
 
-        [HttpGet]   
-        public IEnumerable<Product>GetAllProducts() 
+
+        // Method 2 - Use an IactionResult
+        //[HttpGet]
+        //public IActionResult GetAllProducts()
+        //{
+        //    return Ok(_context.Products.ToArray());
+        //}
+
+        // Give the id a data type.  If the data is incorrect the response is gona be a 404
+        //[HttpGet("{id:int}")]
+        //public IActionResult GetProduct(int id)
+        //{
+        //    var product = _context.Products.Find(id);
+        //    if (product== null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(product);
+
+        //}
+
+
+        [HttpGet]
+        //to get all products GetAllProducts().  With Size and pagination (QueryParameters...)
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
         {
-            return _context.Products.ToArray();
+            IQueryable<Product> products = _context.Products;
+
+            if (queryParameters.MinPrice !=null &&
+                queryParameters.MaxPrice != null)
+            {
+                products = products.Where(
+                    p => p.Price >= queryParameters.MinPrice.Value &&
+                    p.Price <= queryParameters.MaxPrice.Value);
+            }
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(queryParameters.Name.ToLower()));
+            }
+            // To do the pagination 
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+
+            return Ok(await products.ToArrayAsync());
         }
+
+        // Give the id a data type.  If the data is incorrect the response is gona be a 404
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+
+        }
+
     }
 }
